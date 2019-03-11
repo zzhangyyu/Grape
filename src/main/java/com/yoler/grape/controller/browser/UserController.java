61a4d25d57335031f17b2cf0d9d19ff530b468d9
zhangyu <l>
2019-03-11 18:21:31 +0800

    增加就诊日期相关的单选框

--- src/main/java/com/yoler/grape/controller/browser/UserController.java
+++ src/main/java/com/yoler/grape/controller/browser/UserController.java
@@ -1,10 +1,13 @@
 package com.yoler.grape.controller.browser;
 
+import com.yoler.grape.util.DateFormatUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * Created by zhangyu on 2019/3/10 12:50
@@ -15,6 +18,7 @@ public class UserController {
 
     @RequestMapping(value = "registerResult", method = RequestMethod.POST)
     public String registerResult(HttpServletRequest request) {
+        String visitDate = request.getParameter("visitDate");
         String patientName = request.getParameter("patientName");
         String patientSex = request.getParameter("patientSex");
         String patientAge = request.getParameter("patientAge");
@@ -31,7 +35,13 @@ public class UserController {
     }
 
     @RequestMapping(value = "register", method = RequestMethod.GET)
-    public String register() {
+    public String register(Model model) {
+        String thisWeekTuesdayDate = DateFormatUtil.getThisWeek(new Date(), 1) + "（周二）";
+        String thisWeekThursdayDate = DateFormatUtil.getThisWeek(new Date(), 3) + "（周四）";
+        String thisWeekSundayDate = DateFormatUtil.getThisWeek(new Date(), 6) + "（周日）";
+        model.addAttribute("thisWeekTuesdayDate", thisWeekTuesdayDate);
+        model.addAttribute("thisWeekThursdayDate", thisWeekThursdayDate);
+        model.addAttribute("thisWeekSundayDate", thisWeekSundayDate);
         return "register";
     }
 
--- src/main/java/com/yoler/grape/util/DateFormatUtil.java
+++ src/main/java/com/yoler/grape/util/DateFormatUtil.java
@@ -38,7 +38,8 @@ public class DateFormatUtil {
 
         YEAR_MONTH("yyyy-MM"),
 
-        HOURE_MINUTE("HH:mm"),;
+        HOURE_MINUTE("HH:mm"),
+        ;
 
         private String value;
 
@@ -174,5 +175,31 @@ public class DateFormatUtil {
 
         }
         return null;
+    }
+
+
+    public static Date getThisWeekMonday(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 获得当前日期是一个星期的第几天
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+        // 判断要计算的日期是否是周日，如果是则减一天计算周六的，否则会出问题，计算到下一周去了
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期是一个星期的第几天
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        // 根据日历的规则，给当前日期减去（当前星期和一个星期的第一天偏移）
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+        return cal.getTime();
+    }
+
+    public static String getThisWeek(Date date, int num) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(getThisWeekMonday(date));
+        cal.add(Calendar.DATE, num);
+        return formatDate(cal.getTime());
     }
 }
